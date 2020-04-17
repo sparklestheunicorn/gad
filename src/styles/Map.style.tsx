1895ac7e04b798e9eb00d2cd023bf798db1606ab
Presley <presleyanne@gmail.com>
2020-04-17 12:12:15 -0400

    Delete bgPosition since bg disappears now

--- src/styles/Map.style.tsx
+++ src/styles/Map.style.tsx
@@ -1,7 +1,3 @@
-export const bgPosition = depth => ({
-  backgroundPositionX: `calc(100% - ${depth *30}px)`
-})
-
 export const transform = depth => ({
   transform: `translateX(${-40 * depth + 4}%)`
 })
