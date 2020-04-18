f6d3088207ad2bcc06d831f0babc7666187f2503
Presley Pizzo <presleyanne@gmail.com>
2020-04-18 19:04:07 -0400

    Move intro text, make responsive (#9)

--- src/styles/MapQuestions.style.ts
+++ src/styles/MapQuestions.style.ts
@@ -1,24 +1,28 @@
 import { css } from '@emotion/core'
 import { mq, textSize } from '../styles/variables'
 
+export const responsiveFlex = mq({
+  label: 'responsiveFlex',
+  display: 'flex',
+  flexDirection: ['column', 'row', 'row'],
+  alignItems: ['center', null, null],
+})
+
 export const questionList = mq({
   label: 'questionList',
   position: 'relative',
-  width: ['80%', '40%', '40%']
+  width: ['80%', '40%', '40%'],
 })
 
-// was doubled
-// needs css function for types to work
-export const mapIntroContainer = css({
-  label: 'mapIntroContainer',
-  position: 'absolute',
-  top: 0,
-  right: '10%',
-  width: '40%',
-  'h2': {
-    fontSize: textSize.L
-  },
-  'h3': {
-    padding: 0
-  }
-})
+export const mapIntroContainer = css(
+  mq({
+    label: 'mapIntroContainer',
+    width: ['80%', '40%', '40%'],
+    h2: {
+      fontSize: textSize.L,
+    },
+    h3: {
+      padding: 0,
+    },
+  }),
+)
