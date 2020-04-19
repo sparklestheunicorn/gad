411e2f170cf98940096ccf241eeae34224f33d29
Presley <presleyanne@gmail.com>
2020-04-19 18:41:31 -0400

    Refactor mapNode styles to apply theme once

--- src/components/MapNode.style.ts
+++ src/components/MapNode.style.ts
@@ -6,87 +6,81 @@ const rectangle = (theme) =>
     borderRadius: `${theme.spacing.XS}`,
   })
 
-export const mapQuestion = (theme) => [
-  rectangle(theme),
-  css({
-    label: 'mapQuestion',
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'row',
-    marginBottom: theme.spacing.M,
-    cursor: 'pointer',
-  }),
-]
-
-export const mapNode = (theme) => [
-  rectangle(theme),
-  {
-    label: 'mapNode',
-    position: 'unset',
+export const styles = (theme) => ({
+  mapQuestion: [
+    rectangle(theme),
+    css({
+      label: 'mapQuestion',
+      position: 'relative',
+      display: 'flex',
+      flexDirection: 'row',
+      marginBottom: theme.spacing.M,
+      cursor: 'pointer',
+    }),
+  ],
+  mapNode: [
+    rectangle(theme),
+    {
+      label: 'mapNode',
+      position: 'unset',
+      width: '100%',
+      zIndex: '10',
+      padding: `${theme.spacing.S} ${theme.spacing.L} ${theme.spacing.S} ${theme.spacing.S}`,
+      marginBottom: theme.spacing.M,
+      cursor: 'pointer',
+      WebkitFontSmoothing: 'antialiased',
+    },
+  ],
+  mapNodeChildren: css({
+    label: 'nodeChildren',
+    position: 'absolute',
+    left: '100%',
+    top: 0,
+    listStyleType: 'none',
     width: '100%',
-    zIndex: '10',
-    padding: `${theme.spacing.S} ${theme.spacing.L} ${theme.spacing.S} ${theme.spacing.S}`,
-    marginBottom: theme.spacing.M,
-    cursor: 'pointer',
-    WebkitFontSmoothing: 'antialiased',
-  },
-]
-
-export const mapNodeChildren = css({
-  label: 'nodeChildren',
-  position: 'absolute',
-  left: '100%',
-  top: 0,
-  listStyleType: 'none',
-  width: '100%',
-  height: '100%',
-  opacity: 0,
-  animationName: 'fade-in',
-  animationDelay: '0.4s',
-  animationDuration: '1s',
-  animationFillMode: 'forwards',
-})
-
-export const expanded = (isExpanded) =>
-  isExpanded
-    ? css({
-        label: 'expanded',
-        fontWeight: 'bold',
-      })
-    : {}
-
-export const canExpand = (hasChildren, theme) =>
-  hasChildren
-    ? css({
-        label: 'canExpand',
-        '&:before': {
-          content: '"+"',
-          position: 'absolute',
-          top: theme.spacing.S,
-          right: theme.spacing.S,
-          fontSize: theme.textSize.M,
-          lineHeight: theme.textSize.M,
-        },
-      })
-    : {}
-
-export const selectedAndCanExpand = (isExpanded, hasChildren, theme) =>
-  isExpanded && hasChildren
-    ? css({
-        '&:before': {
-          content: '"▻"',
-          position: 'absolute',
-          right: 0,
-          fontWweight: 'bold',
-          fontSize: theme.textSize.M,
-          lineHeight: `calc(${theme.textSize.M} - 3px)`,
-          transform: 'scalex(0.5)',
-        },
-      })
-    : {}
-
-export const questionTitle = (theme) =>
-  css({
+    height: '100%',
+    opacity: 0,
+    animationName: 'fade-in',
+    animationDelay: '0.4s',
+    animationDuration: '1s',
+    animationFillMode: 'forwards',
+  }),
+  expanded: (isExpanded) =>
+    isExpanded
+      ? css({
+          label: 'expanded',
+          fontWeight: 'bold',
+        })
+      : {},
+  canExpand: (hasChildren) =>
+    hasChildren
+      ? css({
+          label: 'canExpand',
+          '&:before': {
+            content: '"+"',
+            position: 'absolute',
+            top: theme.spacing.S,
+            right: theme.spacing.S,
+            fontSize: theme.textSize.M,
+            lineHeight: theme.textSize.M,
+          },
+        })
+      : {},
+  selectedAndCanExpand: (isExpanded, hasChildren) =>
+    isExpanded && hasChildren
+      ? css({
+          '&:before': {
+            content: '"▻"',
+            position: 'absolute',
+            right: 0,
+            fontWweight: 'bold',
+            fontSize: theme.textSize.M,
+            lineHeight: `calc(${theme.textSize.M} - 3px)`,
+            transform: 'scalex(0.5)',
+          },
+        })
+      : {},
+  questionTitle: css({
     label: 'mapQuestionTitle',
     display: 'flex',
     flexDirection: 'row',
@@ -96,8 +90,8 @@ export const questionTitle = (theme) =>
     fontFamily: theme.font.paragraph,
     margin: `0 0 0 ${theme.spacing.M}`,
     paddingRight: theme.spacing.M,
-  })
-
-export const nodeTitle = (theme) => ({
-  fontFamily: theme.font.paragraph,
+  }),
+  nodeTitle: {
+    fontFamily: theme.font.paragraph,
+  },
 })
--- src/components/MapNode.tsx
+++ src/components/MapNode.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 import React from 'react'
-import * as styles from './MapNode.style'
+import { styles } from './MapNode.style'
 import { dropShadow, selected } from '../styles/shared.style'
 import { useTheme } from 'emotion-theming'
 import { Theme } from '@emotion/types'
@@ -11,19 +11,15 @@ export const MapNode = (props) => {
   const { topLevel, title, nodeId, nodeChildren, depth, setMapDepth, setMaxMapDepth, isExpanded, setIsExpanded } = props
 
   const theme: Theme = useTheme()
+  const s = styles(theme)
 
   const [expandedChild, setExpandedChild] = React.useState(null)
 
   const hasChildren = Object.keys(nodeChildren).length > 0
 
   const liStyles = topLevel
-    ? [styles.mapQuestion(theme)]
-    : [
-        styles.mapNode(theme),
-        styles.expanded(isExpanded),
-        styles.canExpand(hasChildren, theme),
-        styles.selectedAndCanExpand(isExpanded, hasChildren, theme),
-      ]
+    ? [s.mapQuestion]
+    : [s.mapNode, s.expanded(isExpanded), s.canExpand(hasChildren), s.selectedAndCanExpand(isExpanded, hasChildren)]
   return (
     <>
       <li
@@ -47,10 +43,10 @@ export const MapNode = (props) => {
         }}
       >
         {topLevel && <ConvoCount numberConvos={Object.keys(nodeChildren).length} />}
-        {topLevel ? <h3 css={styles.questionTitle(theme)}>{title}</h3> : <h4 css={styles.nodeTitle(theme)}>{title}</h4>}
+        {topLevel ? <h3 css={s.questionTitle}>{title}</h3> : <h4 css={s.nodeTitle}>{title}</h4>}
       </li>
       {isExpanded && hasChildren && (
-        <ul css={styles.mapNodeChildren} key={`${nodeId}-children`}>
+        <ul css={s.mapNodeChildren} key={`${nodeId}-children`}>
           {Object.keys(nodeChildren).map((childNodeKey) => {
             const childNode = nodeChildren[childNodeKey]
             return (
