e9c45185ce94b9bf4641fb6df543e0d0a9464b2a
Mike Kissinger <mis4mike@gmail.com>
2020-04-22 15:08:25 -0700

    Preload questions on welcome page, plus a link back to it (#18)
    * Preload questions on welcome page, plus a link back to it
    
    * removing console.log

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,40 @@ import { jsx } from '@emotion/core'
 import React, { Component } from 'react'
 import { HashRouter, Route } from 'react-router-dom'
 import { ThemeProvider } from 'emotion-theming'
+import { getQuestions, getMapNodeSubtree } from './firestore/firestore'
+import { observer } from 'mobx-react'
 
 import { Welcome } from './pages/Welcome'
 import { Map } from './pages/Map'
 
-import { generateTheme } from './styles/themes/themeGenerator'
 import { initDebateMapServerLink } from './firestore/init-dm-link'
 
+import { generateTheme } from './styles/themes/themeGenerator'
 import { styles } from './styles/App.styles'
 
 initDebateMapServerLink()
 
-class App extends Component {
-  render() {
-    const themeId = process.env.REACT_APP_PROJECT_ID
-    const theme = generateTheme(themeId)
+const App = observer((props) => {
+  const themeId = process.env.REACT_APP_PROJECT_ID
+  const theme = generateTheme(themeId)
+  const s = styles(theme)
 
-    return (
-      <ThemeProvider theme={theme}>
-        <HashRouter basename="/">
-          <div css={[styles(theme)]}>
-            <Route path="/map" component={Map} />
-            <Route exact path="/" component={Welcome} />
-          </div>
-        </HashRouter>
-      </ThemeProvider>
-    )
-  }
-}
+  const questions = getQuestions()
+  const questionChildren = questions.map((question) => ({
+    questionId: question._key,
+    childNodes: getMapNodeSubtree(question._key),
+  }))
+
+  return (
+    <ThemeProvider theme={theme}>
+      <HashRouter basename="/">
+        <div css={s.appContainer}>
+          <Route path="/map" render={() => <Map questions={questions} questionChildren={questionChildren} />} />
+          <Route exact path="/" component={Welcome} />
+        </div>
+      </HashRouter>
+    </ThemeProvider>
+  )
+})
 
 export default App
